1812e5b7f03a90a2c2ccb124043221ac576f2b67
IrisLiz <lizbethirisariastinco@gmail.com>
2025-05-28 15:47:24 -0500

    Mejoras en botones y tema claro/oscuro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, CommonModule, NzIconModule],
+  template: `
+    <router-outlet></router-outlet>
+    <div class="theme-toggle" (click)="toggleTheme()">
+      <i nz-icon [nzType]="isDarkTheme ? 'sun' : 'moon'" nzTheme="fill"></i>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'Ciskoi-Wallet';
+  isDarkTheme = false;
+
+  constructor() {
+    // Recuperar tema guardado
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+      this.enableDarkTheme();
+    }
+  }
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    if (this.isDarkTheme) {
+      this.enableDarkTheme();
+    } else {
+      this.enableLightTheme();
+    }
+  }
+
+  private enableDarkTheme() {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+    this.isDarkTheme = true;
+  }
+
+  private enableLightTheme() {
+    document.documentElement.removeAttribute('data-theme');
+    localStorage.setItem('theme', 'light');
+    this.isDarkTheme = false;
+  }
 }
